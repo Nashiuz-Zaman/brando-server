e0e86496f7f8571bce9a269764211a7c58bb8e03
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-21 20:31:06 +0600

    Refactor code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ejmezk0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -83,19 +83,25 @@ async function run() {
 
     // api for setting single product
     app.put("/brands/:brandId/products/update/:productId", async (req, res) => {
+      //  get the brand and the product
       const brandId = req.params.brandId;
       const productId = req.params.productId;
+      // get the collection
       const collection = database.collection(`${brandId}`);
       const objectId = new ObjectId(productId);
+      // build the query
       const filter = { _id: objectId };
 
+      // set the upsert option to true
       const options = { upsert: true };
+      // get the updated product from request body
       const updatedProduct = req.body;
 
+      // create the updated information
       const updateDoc = {
         $set: updatedProduct,
       };
-
+      // update the document and send result
       const result = await collection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
@@ -110,22 +116,31 @@ async function run() {
 
     // api for search specific email's cart products
     app.post("/cart", async (req, res) => {
+      // retrieve the email from the body
       const email = req.body.email;
       const collection = database.collection("cart");
+      // make the query
       const query = { addedBy: email };
+      // get the cursor
       const cursor = collection.find(query);
+      // conver to array
       const result = await cursor.toArray();
-      console.log(result);
+      // send the result
       res.send(result);
     });
 
     // api to delete specific cart product
     app.delete("/cart/:id", async (req, res) => {
+      //retrieve the id
       const item = req.params.id;
       const objectId = new ObjectId(item);
+      //  build the query
       const filter = { _id: objectId };
+      // get the collection
       const collection = database.collection("cart");
+      // delete the document
       const result = await collection.deleteOne(filter);
+      // send the result
       res.send(result);
     });
 
